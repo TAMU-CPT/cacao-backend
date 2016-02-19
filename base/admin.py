f0813e0b839763c3ecb77ea6b63ca78492f7186d
Eleni Mijalis <elenimijalis@gmail.com>
2016-02-19 00:00:50 -0600

    admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Team, GAF, Annotation, Challenge, Assessment
 
-# Register your models here.
+admin.site.register(Team)
+admin.site.register(GAF)
+admin.site.register(Annotation)
+admin.site.register(Challenge)
+admin.site.register(Assessment)

ae98c54c6d888fc7e5b8497a5c3031bc0865d1b9
Eleni Mijalis <elenimijalis@gmail.com>
2016-04-24 14:43:44 -0500

    requst timeout increased

--- cacao/settings.py
+++ cacao/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -53,6 +54,10 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
     'PAGE_SIZE': 10
+}
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
 }
 
 MIDDLEWARE_CLASSES = [

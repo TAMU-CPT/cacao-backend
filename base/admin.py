e9276ca118916883e5a7ddfb25351ab9f8c30429
Eric Rasche <rasche.eric@gmail.com>
2016-07-22 09:06:25 +0000

    Easier admin

--- base/admin.py
+++ base/admin.py
@@ -4,8 +4,16 @@ from .models import GAF, Challenge, Assessment, Paper
 class GAFAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner', 'db', 'qualifier', 'review_state', 'go_id', 'db_reference', 'date')
 
+class ChallengeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'gaf', 'date')
+
+class AssessmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'gaf', 'challenge', 'date')
+
+class PaperAdmin(admin.ModelAdmin):
+    list_display = ('pmid', 'pub_year', 'title', 'pmc')
 
 admin.site.register(GAF, GAFAdmin)
-admin.site.register(Challenge)
-admin.site.register(Assessment)
-admin.site.register(Paper)
+admin.site.register(Challenge, ChallengeAdmin)
+admin.site.register(Assessment, AssessmentAdmin)
+admin.site.register(Paper, PaperAdmin)

4e7a947b5a29baacb597c6d4820ded1386b20da3
Eleni Mijalis <elenimijalis@gmail.com>
2016-08-22 16:22:38 -0500

    didn't need to add

--- base/views.py
+++ base/views.py
@@ -134,6 +134,6 @@ def mark_all_read(request):
     Mark all messages as read (i.e. delete from inbox) for current logged in user
     """
     from stored_messages.settings import stored_messages_settings
-    backend = stored_messages_settings.stored_messages_settings.STORAGE_BACKEND()
+    backend = stored_messages_settings.STORAGE_BACKEND()
     backend.inbox_purge(request.user)
     return Response({"message": "All messages read"})

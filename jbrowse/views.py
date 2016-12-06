5921653a408f27a5ee295c80b9912284ed46e193
Eric Rasche <rasche.eric@gmail.com>
2016-12-06 00:48:31 +0000

    catch errors more nicely?

--- jbrowse/views.py
+++ jbrowse/views.py
@@ -12,8 +12,16 @@ def feature_data(request, name=None):
     q_org = request.GET.get('organism')
     q_start = request.GET.get('start')
     q_end = request.GET.get('end')
-    org = Organism.objects.get(common_name=q_org)
-    refseq = RefSeq.objects.get(name=name, organism=org)
+    try:
+        org = Organism.objects.get(common_name=q_org)
+    except Organism.DoesNotExist:
+        return JsonResponse({})
+
+    try:
+        refseq = RefSeq.objects.get(name=name, organism=org)
+    except Organism.DoesNotExist:
+        return JsonResponse({})
+
     genes = Gene.objects.filter(refseq=refseq, start__lte=q_end, end__gte=q_start)
     data = {'features':[]}
     for gene in genes:
